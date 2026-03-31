19d7e9908b7563c3b47e29b76721ad18c7fc8aba
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-03-31 11:15:44 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -18,4 +18,13 @@ def grades():
         Para la entrada "3", la salida esperada es:
         Insuficiente
     """
-    pass
+     notas = int(input())
+    if notas >= 9 and notas <= 10:
+        print("Excelente")
+    elif notas >= 7 and notas <= 8:
+        print("Bueno")
+    elif notas >= 5 and notas <= 6:
+        print("Regular")
+    elif notas >= 0 and notas <= 4:
+        print("Insuficiente")
+
